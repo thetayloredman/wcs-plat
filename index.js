3368d4ad47364a7094297a2d4bb6fd8d76401a92
thetayloredman <nutdriver716@gmail.com>
2020-10-06 12:06:53 -0700

    aaa

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+// wcs-plat
+// Copyright (C) 2020  Logan Devine
+exports.__esModule = true;
+// Imports
+require("enmap");
+require("morgan");
---
+++ index.ts
@@ -0,0 +1,8 @@
+// wcs-plat
+// Copyright (C) 2020  Logan Devine
+
+// Imports
+import 'enmap';
+import 'morgan';
+import * as express from 'express';
+import * as _ from 'lodash';
