7d4be40972d07d8e2d8be1aaf692a45a6434aef3
thetayloredman <nutdriver716@gmail.com>
2020-10-06 13:49:39 -0700

    Lol

--- constants/port/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// wcs-plat
-// Copyright (C) 2020  Logan Devine
-Object.defineProperty(exports, "__esModule", { value: true });
-// Constants/port
-var value = 8080;
-// Export
-exports.default = value;
